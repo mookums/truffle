6a08430cd8db21fbeb38223c39ad0ffe2d023aa9
Muki Kiboigo <muki@muki.gg>
2025-07-18 12:51:34 -0700

    feat(delete): basic delete support

---
+++ truffle-sim/src/action/delete.rs
@@ -0,0 +1,104 @@
+use sqlparser::ast::{Delete, FromTable, TableFactor};
+
+use crate::{Error, Simulator, object_name_to_strings, ty::SqlType};
+
+use super::join::JoinInferrer;
+
+impl Simulator {
+    pub(crate) fn delete(&self, delete: Delete) -> Result<(), Error> {
+        // TODO: Support multi table deletes (for MySQL)
+
+        let mut contexts = vec![];
+
+        match delete.from {
+            FromTable::WithFromKeyword(tables_with_joins) => {
+                for from in tables_with_joins {
+                    // TODO: Remove this duplication.
+                    let TableFactor::Table { name, alias, .. } = &from.relation else {
+                        return Err(Error::Unsupported(
+                            "Unsupported DELETE relation".to_string(),
+                        ));
+                    };
+                    let from_table_name = object_name_to_strings(name).first().unwrap().clone();
+                    let from_table_alias = alias.as_ref().map(|a| a.name.value.clone());
+
+                    let from_table = self
+                        .get_table(&from_table_name)
+                        .ok_or_else(|| Error::TableDoesntExist(from_table_name.clone()))?;
+
+                    if let Some(alias) = &from_table_alias {
+                        if self.has_table(alias) {
+                            return Err(Error::AliasIsTableName(alias.to_string()));
+                        }
+                    }
+
+                    let join_table = self.infer_joins(
+                        from_table,
+                        &from_table_name,
+                        from_table_alias.as_ref(),
+                        &from.joins,
+                    )?;
+
+                    contexts.push(join_table);
+                }
+            }
+            FromTable::WithoutKeyword(_) => {
+                return Err(Error::Unsupported(
+                    "DELETE FROM without FROM keyword".to_string(),
+                ));
+            }
+        }
+
+        let inferrer = JoinInferrer {
+            join_contexts: &contexts,
+        };
+
+        if let Some(selection) = delete.selection {
+            let ty = self.infer_expr_type(&selection, Some(SqlType::Boolean), &inferrer)?;
+
+            if ty != SqlType::Boolean {
+                return Err(Error::TypeMismatch {
+                    expected: SqlType::Boolean,
+                    got: ty,
+                });
+            }
+        }
+
+        Ok(())
+    }
+}
+
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn delete_row_by_field() {
+        let mut sim = Simulator::new(Box::new(GenericDialect {}));
+        sim.execute("create table person (id int primary key, name text)")
+            .unwrap();
+        sim.execute("delete from person where id = ?").unwrap();
+        sim.execute("delete from person where id = 5").unwrap();
+    }
+
+    #[test]
+    fn delete_row_column_doesnt_exist() {
+        let mut sim = Simulator::new(Box::new(GenericDialect {}));
+        sim.execute("create table person (id int primary key, name text)")
+            .unwrap();
+
+        assert_eq!(
+            sim.execute("delete from person where weight = ?"),
+            Err(Error::ColumnDoesntExist("weight".to_string()))
+        )
+    }
+
+    #[test]
+    fn delete_row_table_doesnt_exist() {
+        let mut sim = Simulator::new(Box::new(GenericDialect {}));
+
+        assert_eq!(
+            sim.execute("delete from person where weight = ?"),
+            Err(Error::TableDoesntExist("person".to_string()))
+        )
+    }
+}
