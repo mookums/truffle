e1d2ae52736ad97a7967a79e0cc2060ec5d7adf9
Muki Kiboigo <muki@muki.gg>
2025-08-03 18:33:35 -0700

    feat(sqlx): type checking on sqlx macro

---
+++ truffle-sqlx-demo/migrations/01_create_account_table.sql
@@ -0,0 +1,7 @@
+create table account (
+    id int primary key,
+    name text not null,
+    email text not null,
+    password text not null,
+    status integer not null
+);
---
+++ truffle-sqlx-demo/src/main.rs
@@ -0,0 +1,47 @@
+use std::str::FromStr;
+
+use sqlx::sqlite::SqliteConnectOptions;
+use sqlx::sqlite::SqlitePool;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum AccountStatus {
+    Active = 1,
+    Inactive = 2,
+    Deleted = 3,
+}
+
+impl From<AccountStatus> for i32 {
+    fn from(value: AccountStatus) -> Self {
+        match value {
+            AccountStatus::Active => 1,
+            AccountStatus::Inactive => 2,
+            AccountStatus::Deleted => 3,
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let opts = SqliteConnectOptions::from_str("./test.db")
+        .unwrap()
+        .create_if_missing(true);
+    let db = SqlitePool::connect_with(opts).await.unwrap();
+
+    sqlx::migrate!().run(&db).await.unwrap();
+
+    truffle_sqlx::query!(
+        (),
+        r#"
+        insert into account
+        values (?, ?, ?, ?, ?);
+        "#,
+        0,
+        "John Doe",
+        "johndoe@example.com",
+        "password1",
+        AccountStatus::Active,
+    )
+    .execute(&db)
+    .await
+    .unwrap();
+}
