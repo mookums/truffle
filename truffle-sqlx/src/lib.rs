4dc78e8ef45702702c80e7e40dd40be6e35bf69b
Muki Kiboigo <muki@muki.gg>
2025-08-07 15:14:21 -0700

    feat(sqlx): result query types

--- truffle-sqlx/src/lib.rs
+++ truffle-sqlx/src/lib.rs
@@ -1,10 +1,10 @@
 use proc_macro::{Span, TokenStream};
 use quote::quote;
-use std::sync::LazyLock;
-use syn::{
-    Error, Token,
-    parse::{Parse, discouraged::Speculative},
+use std::{
+    hash::{DefaultHasher, Hash, Hasher},
+    sync::LazyLock,
 };
+use syn::{Error, Token, parse::Parse};
 use truffle::{Simulator, ty::SqlType};
 use truffle_loader::{
     config::load_config,
@@ -26,27 +26,13 @@ static SIMULATOR: LazyLock<Result<Simulator, Error>> = LazyLock::new(|| {
 });
 
 struct QueryInput {
-    ty: Option<syn::Type>,
     sql_lit: syn::LitStr,
     placeholders: Vec<syn::Expr>,
 }
 
 impl Parse for QueryInput {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let fork = input.fork();
-
-        let (ty, sql_lit) = fork
-            .parse::<syn::Type>()
-            .ok()
-            .and_then(|ty| {
-                fork.parse::<Token![,]>().ok().and_then(|_| {
-                    fork.parse::<syn::LitStr>().ok().map(|sql_lit| {
-                        input.advance_to(&fork);
-                        (Some(ty), sql_lit)
-                    })
-                })
-            })
-            .unwrap_or_else(|| (None, input.parse().unwrap()));
+        let sql_lit = input.parse().unwrap();
 
         let placeholders: Vec<_> = if input.is_empty() {
             Vec::new()
@@ -61,7 +47,6 @@ impl Parse for QueryInput {
         };
 
         Ok(QueryInput {
-            ty,
             sql_lit,
             placeholders,
         })
@@ -113,17 +98,6 @@ pub fn query(input: TokenStream) -> TokenStream {
                 _ => quote! { let #binding = #rust_expr; },
             };
 
-            // let conversion = match sql_type {
-            //     SqlType::SmallInt => quote! { let #binding: i16 = (#rust_expr); },
-            //     SqlType::Integer => quote! { let #binding: i32 = (#rust_expr); },
-            //     SqlType::BigInt => quote! { let #binding: i64 = (#rust_expr); },
-            //     SqlType::Float => quote! { let #binding: f32 = (#rust_expr); },
-            //     SqlType::Double => quote! { let #binding: f64 = (#rust_expr); },
-            //     SqlType::Text => quote! { let #binding: &str = (#rust_expr); },
-            //     SqlType::Boolean => quote! { let #binding: bool = (#rust_expr); },
-            //     _ => quote! { let #binding = #rust_expr; },
-            // };
-
             (conversion, binding)
         })
         .collect::<Vec<_>>();
@@ -165,84 +139,102 @@ pub fn query_as(input: TokenStream) -> TokenStream {
             .into();
     }
 
-    // Create type checks.
-    let type_checks: Vec<_> = resolve
+    let bindings: Vec<_> = resolve
         .input_iter()
         .zip(parsed.placeholders.iter())
         .enumerate()
-        .map(|(i, (sql_type, rust_type))| {
-            let check_fn = syn::Ident::new(&format!("_check_param_{i}"), Span::call_site().into());
+        .map(|(i, (sql_type, rust_expr))| {
+            let binding = syn::Ident::new(&format!("_arg_{i}"), Span::call_site().into());
 
-            match sql_type {
-                SqlType::SmallInt => quote! {
-                    fn #check_fn<T>() where T: Into<i16> {}
-                    #check_fn::<#rust_type>();
-                },
-                SqlType::Integer => quote! {
-                    fn #check_fn<T>() where T: Into<i32> {}
-                    #check_fn::<#rust_type>();
-                },
-                SqlType::BigInt => quote! {
-                    fn #check_fn<T>() where T: Into<i64> {}
-                    #check_fn::<#rust_type>();
-                },
-                SqlType::Float => quote! {
-                    fn #check_fn<T>() where T: Into<f32> {}
-                    #check_fn::<#rust_type>();
-                },
-                SqlType::Double => quote! {
-                    fn #check_fn<T>() where T: Into<f64> {}
-                    #check_fn::<#rust_type>();
-                },
-                SqlType::Text => quote! {
-                    fn #check_fn<T>() where T: ToString {}
-                    #check_fn::<#rust_type>();
-                },
-                SqlType::Boolean => quote! {
-                    fn #check_fn<T>() where T: Into<bool> {}
-                    #check_fn::<#rust_type>();
-                },
-                _ => quote! {},
+            let conversion = match sql_type {
+                SqlType::SmallInt => quote! { let #binding: i16 = (#rust_expr).into(); },
+                SqlType::Integer => quote! { let #binding: i32 = (#rust_expr).into(); },
+                SqlType::BigInt => quote! { let #binding: i64 = (#rust_expr).into(); },
+                SqlType::Float => quote! { let #binding: f32 = (#rust_expr).into(); },
+                SqlType::Double => quote! { let #binding: f64 = (#rust_expr).into(); },
+                SqlType::Text => quote! { let #binding: String = (#rust_expr).to_string(); },
+                SqlType::Boolean => quote! { let #binding: bool = (#rust_expr).into(); },
+                _ => quote! { let #binding = #rust_expr; },
+            };
+
+            (conversion, binding)
+        })
+        .collect::<Vec<_>>();
+
+    let (conversions, binding_names): (Vec<_>, Vec<_>) = bindings.into_iter().unzip();
+
+    let result_fields: Vec<_> = resolve
+        .output_iter()
+        .map(|(name, col)| {
+            let base_type = match col.ty {
+                SqlType::SmallInt => quote!(i16),
+                SqlType::Integer => quote!(i32),
+                SqlType::BigInt => quote!(i64),
+                SqlType::Float => quote!(f32),
+                SqlType::Double => quote!(f64),
+                SqlType::Text => quote!(String),
+                SqlType::Boolean => quote!(bool),
+                _ => panic!("Unsupported Type"),
+            };
+
+            let true_type = if col.nullable {
+                quote! { Option<#base_type> }
+            } else {
+                base_type
+            };
+
+            let field_name = syn::Ident::new(&name.name, Span::call_site().into());
+
+            quote! {
+                pub #field_name: #true_type,
             }
         })
         .collect();
 
-    // Run your SQL.
-    match parsed.ty {
-        Some(ty) => TokenStream::from(quote! {
-            #(#type_checks)*
-            sqlx::query_as::<_, #ty>(#sql)
-        }),
-        None => TokenStream::from(quote! {
-            #(#type_checks)*
-            sqlx::query_as(#sql)
-        }),
-    }
-}
+    let mut hasher = DefaultHasher::new();
+    sql.hash(&mut hasher);
+    let hashed = hasher.finish();
 
-#[proc_macro]
-pub fn query_scalar(input: TokenStream) -> TokenStream {
-    let parsed = syn::parse_macro_input!(input as QueryInput);
-    let sql = parsed.sql_lit.value();
-
-    let mut sim = match SIMULATOR.as_ref() {
-        Ok(simulator) => simulator.clone(),
-        Err(e) => return e.to_compile_error().into(),
-    };
-
-    if let Err(e) = sim.execute(&sql) {
-        return Error::new(parsed.sql_lit.span(), e.to_string())
-            .to_compile_error()
-            .into();
-    }
+    let result_struct_name =
+        syn::Ident::new(&format!("QueryResult_{hashed}"), Span::call_site().into());
 
     // Run your SQL.
-    match parsed.ty {
-        Some(ty) => TokenStream::from(quote! {
-            sqlx::query_scalar::<_, #ty>(#sql)
-        }),
-        None => TokenStream::from(quote! {
-            sqlx::query_scalar(#sql)
-        }),
-    }
+    TokenStream::from(quote! {
+        {
+            #[derive(Debug, Clone, sqlx::FromRow)]
+            pub struct #result_struct_name {
+                #(#result_fields)*
+            }
+
+            #(#conversions)*
+            sqlx::query_as::<_, #result_struct_name>(#sql)#(.bind(#binding_names))*
+        }
+    })
 }
+
+// #[proc_macro]
+// pub fn query_scalar(input: TokenStream) -> TokenStream {
+//     let parsed = syn::parse_macro_input!(input as QueryInput);
+//     let sql = parsed.sql_lit.value();
+
+//     let mut sim = match SIMULATOR.as_ref() {
+//         Ok(simulator) => simulator.clone(),
+//         Err(e) => return e.to_compile_error().into(),
+//     };
+
+//     if let Err(e) = sim.execute(&sql) {
+//         return Error::new(parsed.sql_lit.span(), e.to_string())
+//             .to_compile_error()
+//             .into();
+//     }
+
+//     // Run your SQL.
+//     match parsed.ty {
+//         Some(ty) => TokenStream::from(quote! {
+//             sqlx::query_scalar::<_, #ty>(#sql)
+//         }),
+//         None => TokenStream::from(quote! {
+//             sqlx::query_scalar(#sql)
+//         }),
+//     }
+// }
